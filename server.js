53761c8cfda5d21d55fcbe8519afeaa1bf98a181
Cromuel Barut <cromuelbarut@gmail.com>
2021-01-07 14:48:37 +0800

    prepared for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -12,6 +13,16 @@ app.use(express.json({ extended: false }));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/items', require('./routes/api/item'));
+
+// serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+	// set statis folder
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 const port = process.env.PORT || 5000;
 
